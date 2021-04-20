5f90d5723e8ad9eca64c6a37381b05d956eef0d1
antrikshmisri <antrikshmisri@gmail.com>
2021-04-20 20:02:55 +0530

    feat: added firebase service

---
+++ src/firebase.js
@@ -0,0 +1,31 @@
+import firebase from 'firebase'
+import 'firebase/auth'
+
+firebase.initializeApp({
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+})
+
+
+
+
+export const auth = firebase.auth()
+
+const googleProvider = new firebase.auth.GoogleAuthProvider()
+
+export const signInWithGoogle = () => {
+    auth.signInWithPopup(googleProvider)
+    .then(res => {console.log(res.user)})
+    .catch(err => {console.log(err.message)})
+}
+
+export const signOut = () => {
+    auth.signOut()
+    .then(console.log('Logged Out'))
+    .catch(err => {console.log(err.message)})
+}
