c4f4fa5adf4d4d69e61072cf8acbbee2afcd24e9
antrikshmisri <antrikshmisri@gmail.com>
2021-08-28 20:41:19 +0530

    feat: component for textfield

---
+++ src/components/TextField.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const TextField = (props) => {
+  return (
+    <input
+      required={props.isRequired}
+      placeholder={props.placeholder}
+      name={props.name}
+      onChange={props.onChange}
+      value = {props.value}
+    >
+      {props.children}
+    </input>
+  );
+};
+
+export default TextField;
