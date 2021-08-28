561b229bcb79dd323eed1ad1c8ea3e8099ea3262
antrikshmisri <antrikshmisri@gmail.com>
2021-08-28 20:42:12 +0530

    feat: sweet alert messages

---
+++ src/constants/messages.js
@@ -0,0 +1,26 @@
+import NoteInputs from "../components/NoteInputs";
+
+const messages = {
+  noteinfo: {
+    confirmButtonText: "Add",
+    showCancelButton: true,
+    cancelButtonText: "Cancel",
+    preConfirm: () => {
+      return new Promise((resolve) => {
+        resolve([
+          document.querySelector("input[name = 'title']").value,
+          document.querySelector("textarea[name = 'body']").value,
+        ]);
+      });
+    },
+    html: <NoteInputs />,
+  },
+  nullinfo: {
+      confirmButtonText: "OK",
+      title: "Please add the title",
+      showCancelButton: false,
+      icon: "error",
+  }
+};
+
+export default messages;
