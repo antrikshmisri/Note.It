e6bee154622572e9fe5014417de8967651043797
antrikshmisri <antrikshmisri@gmail.com>
2021-08-28 20:40:43 +0530

    feat: inputs for note title, body

---
+++ src/components/NoteInputs.js
@@ -0,0 +1,20 @@
+import TextField from "../components/TextField";
+import { Container, Row, Col } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const RepoInputs = () => {
+  return (
+      <Container>
+        <Row className="inputs">
+          <Col md={"12"} className="mb-3">
+            <TextField isRequired={true} placeholder="TITLE" name="title"/>
+          </Col>
+          <Col md={"12"}>
+            <textarea placeholder="BODY" name="body"/>
+          </Col>
+        </Row>
+      </Container>
+  );
+};
+
+export default RepoInputs;
