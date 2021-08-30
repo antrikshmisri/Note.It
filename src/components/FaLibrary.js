12463e3c4b0ba59cf455d47dbc1a3dd884d49a32
antrikshmisri <antrikshmisri@gmail.com>
2021-08-31 01:41:44 +0530

    feat: more icons!

--- src/components/FaLibrary.js
+++ src/components/FaLibrary.js
@@ -1,10 +1,23 @@
 import { library } from "@fortawesome/fontawesome-svg-core";
-import {fab,faGoogle, faGithub, faLinkedin, faInstagram} from "@fortawesome/free-brands-svg-icons";
-import { faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
-
+import {
+  fab,
+  faGoogle,
+  faGithub,
+  faLinkedin,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
+import { faSignOutAlt, faEdit } from "@fortawesome/free-solid-svg-icons";
 
 const initFontAwesome = () => {
-    library.add(fab, faGoogle, faGithub, faLinkedin, faInstagram, faSignOutAlt)
-}
+  library.add(
+    fab,
+    faGoogle,
+    faGithub,
+    faLinkedin,
+    faInstagram,
+    faSignOutAlt,
+    faEdit
+  );
+};
 
-export default initFontAwesome
+export default initFontAwesome;
