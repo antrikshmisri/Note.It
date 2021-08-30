f176f2f16191cfff5c5600020ef8dcc1370caccd
antrikshmisri <antrikshmisri@gmail.com>
2021-08-31 01:42:49 +0530

    feat: context for canvas

---
+++ src/contexts/CanvasContext.js
@@ -0,0 +1,68 @@
+import React, { useContext, useRef, useState } from "react";
+
+const CanvasContext = React.createContext();
+
+export const CanvasProvider = ({ children }) => {
+  const [isDrawing, setIsDrawing] = useState(false);
+  const canvasRef = useRef(null);
+  const contextRef = useRef(null);
+
+  const prepareCanvas = () => {
+    const canvas = canvasRef.current;
+    canvas.className = "canvas";
+    canvas.width = 800;
+    canvas.height = 800;
+    const context = canvas.getContext("2d");
+    context.scale(2, 2);
+    context.lineCap = "round";
+    context.strokeStyle = "black";
+    context.lineWidth = 5;
+    contextRef.current = context;
+  };
+
+  const startDrawing = ({ nativeEvent }) => {
+    const { offsetX, offsetY } = nativeEvent;
+    contextRef.current.beginPath();
+    contextRef.current.moveTo(offsetX, offsetY);
+    setIsDrawing(true);
+  };
+
+  const finishDrawing = () => {
+    contextRef.current.closePath();
+    setIsDrawing(false);
+  };
+
+  const draw = ({ nativeEvent }) => {
+    if (!isDrawing) {
+      return;
+    }
+    const { offsetX, offsetY } = nativeEvent;
+    contextRef.current.lineTo(offsetX, offsetY);
+    contextRef.current.stroke();
+  };
+
+  const clearCanvas = () => {
+    const canvas = canvasRef.current;
+    const context = canvas.getContext("2d");
+    context.fillStyle = "white";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+  };
+
+  return (
+    <CanvasContext.Provider
+      value={{
+        canvasRef,
+        contextRef,
+        prepareCanvas,
+        startDrawing,
+        finishDrawing,
+        clearCanvas,
+        draw,
+      }}
+    >
+      {children}
+    </CanvasContext.Provider>
+  );
+};
+
+export const useCanvas = () => useContext(CanvasContext);
