f7f679f81658e978009fc31a9030c8615fd63980
antrikshmisri <antrikshmisri@gmail.com>
2021-08-31 01:40:50 +0530

    feat: Canvas component

---
+++ src/components/Canvas.js
@@ -0,0 +1,58 @@
+import React, { useEffect } from "react";
+import { Container, Row, Col, Button } from "react-bootstrap";
+import { useCanvas } from "../contexts/CanvasContext";
+import TextField from "./TextField";
+
+export const Canvas = () => {
+  const {
+    canvasRef,
+    prepareCanvas,
+    startDrawing,
+    finishDrawing,
+    draw,
+    clearCanvas,
+  } = useCanvas();
+
+  useEffect(() => {
+    prepareCanvas();
+  }, []);
+
+  return (
+    <Container>
+      <Row>
+        <Col lg={"12"} className="mb-2">
+          <TextField name="title" placeholder="Title" isRequired={true} />
+        </Col>
+        <Col lg={"12"}>
+          <canvas
+            onMouseDown={startDrawing}
+            onMouseUp={finishDrawing}
+            onMouseMove={draw}
+            ref={canvasRef}
+          />
+        </Col>
+        <Col lg={"12"}>
+          <Container>
+            <Row>
+              <Col lg={"2"}>
+                <Button className="sub-btn">
+                  <span>Edit</span>
+                </Button>
+              </Col>
+              <Col lg={"2"}>
+                <Button className="sub-btn" onClick={clearCanvas}>
+                  <span>Clear</span>
+                </Button>
+              </Col>
+              <Col lg={"6"}>
+                <Button className="sub-btn">
+                  <span>Convert To Text</span>
+                </Button>
+              </Col>
+            </Row>
+          </Container>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
